456bf3dbb59622c6ea20219db471704ba5445787
yash sehgal <155468916+realyashsehgal@users.noreply.github.com>
2025-03-20 18:04:17 +0530

    Create MainScreen.java

---
+++ MainScreen.java
@@ -0,0 +1,2 @@
+//This is going to be the main frame isme we will have the intro like name of the application, logo, current active events(optional), and login button 
+//is file mai code aisa hoga like ui, then button or fir unke functions call jo task hona hai like admin login wala button click hoga to admin class ka object banake uske method ko call krenge
