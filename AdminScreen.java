531e3346c35fdf4cc2aad0da5a08c4fafeca5bab
yash sehgal <155468916+realyashsehgal@users.noreply.github.com>
2025-03-20 18:13:48 +0530

    Update AdminScreen.java

--- AdminScreen.java
+++ AdminScreen.java
@@ -1,1 +1,2 @@
 //yae admin screen hogi isme current active events with date and time show honge and ek button hoga add events jo naya frame open krega jisme isme bhi us addevents ki class hogi object banega call hogi wo iusme 
+//ek or button rahega isme make reservation kyuki yae admin based application hai to us button pe click hoga simialrly new class pe jayenge
