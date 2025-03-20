be46f53dd32c04ad6954753778eb882d98c03189
yash sehgal <155468916+realyashsehgal@users.noreply.github.com>
2025-03-20 18:12:38 +0530

    Create AddEvents.java

---
+++ AddEvents.java
@@ -0,0 +1,2 @@
+//yae waali thodi tough hogi isme ui banega with multiple input boxes for event name, time(start end both), date and event type (like cricket game (odi,t20etcetc)yae optional hai) and isme events add hogne data base mae hoga wo abhi dekhna padega kaise work krta hai 
+//and isme dhyaan rkhna hoga ki jo naya event add hoga wo kisi or se clash na ho etc etc yae event add ka button hoga neeche uske baad iska frame band ho jayega or wapas ham add event wale frame pe redirect krenge
