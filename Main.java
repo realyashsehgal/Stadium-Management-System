cf5c63005c1c5556f3b027b7b6f2e5ce95af015c
realyashsehgal <yash.sehgal1234@gmail.com>
2025-03-22 20:59:01 +0530

    basic layout made on main frame

--- Main.java
+++ Main.java
@@ -1,2 +1,10 @@
 //This is going to be the main frame isme we will have the intro like name of the application, logo, current active events(optional), and login button 
 //is file mai code aisa hoga like ui, then button or fir unke functions call jo task hona hai like admin login wala button click hoga to admin class ka object banake uske method ko call krenge
+import java.swing.Jframe;
+public class MainScreen {
+    public static void main(String[] args) {
+        Jframe frame = new Jframe("Main frame");//This is the main frame
+        frame.setSize(500,400);
+        
+    }
+}
